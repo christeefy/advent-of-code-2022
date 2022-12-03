2d284f55b636d6191b0b2fa15e3ef8e0e142f120
Christopher Tee <christopherfytee@gmail.com>
2022-12-03 11:36:31 -0500

    🍱 Add starter code

---
+++ src/bin/day0x.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // const PUZZLE_DATA: &str = include_str!("day0x/puzzle.txt");
+    // println!("Part1: {}", part1(PUZZLE_DATA.to_owned()));
+    // println!("Part2: {}", part2(PUZZLE_DATA.to_owned()));
+}
+
+// fn part1(data: &str) { }
+// fn part2(data: &str) { }
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+    // const DATA: &str = include_str!("day0x/sample.txt");
+
+    #[test]
+    fn test_part1() {
+        // assert_eq!(part1(DATA.to_owned()));
+    }
+
+    #[test]
+    fn test_part2() {
+        // assert_eq!(part2(DATA.to_owned()));
+    }
+}
